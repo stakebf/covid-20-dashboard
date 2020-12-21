24db6b813f59cf8a825ec350dbddaffadb4807c9
maria larchenko <mashalarchenko16@gmail.com>
2020-12-21 17:50:25 +0300

    feat: open popup on hover

--- src/components/Tables/Map/MapContainer.jsx
+++ src/components/Tables/Map/MapContainer.jsx
@@ -1,4 +1,4 @@
-import React, { useEffect, useState } from 'react';
+import React, { useEffect, useState, useRef } from 'react';
 import { MapContainer, TileLayer, useMapEvents, Popup, CircleMarker, useMap } from 'react-leaflet'
 import { makeStyles } from '@material-ui/core/styles';
 import Legend from './Legend'
@@ -6,6 +6,7 @@ import MapProvider from './MapProvider'
 import { getStatistics } from '../../../helpers/getStatistics'
 import 'leaflet/dist/leaflet.css'
 import './map.scss'
+import { Layer } from 'leaflet';
 // import classes from './MapContainer.module.scss';
 
 
@@ -47,8 +48,14 @@ function MapEventHandler({ setZoom, setIsNewLocation }) {
             setZoom(map.getZoom())
         },
         click(e) {
+            console.log(e.target)
             map.locate()
         },
+        focus(e) {
+            console.log(e.target, 'HOVER')
+
+        }
+
         // locationfound(e) {
         //     console.log(e, 'LOCATION E', e.latlng)
         //     map.flyTo([13.87992, 45.9791], map.getZoom())
@@ -67,7 +74,7 @@ function Map({ stat, byCountries, location, pickedCountry }) {
     const [zoom, setZoom] = useState(1.5);
     const [isNewLocation, setIsNewLocation] = useState(false);
     const [statictic, setStatistic] = useState(stat)
-
+    const marker = useRef(null);
     useEffect(() => {
         setAllCases(byCountries);
     }, [byCountries])
@@ -75,6 +82,8 @@ function Map({ stat, byCountries, location, pickedCountry }) {
     useEffect(() => {
         setLocation(location)
         setPickedCountry(pickedCountry)
+        console.log(marker, "';V;F'S'")
+
     }, [location, newLocation, country, pickedCountry])
 
 
@@ -84,7 +93,7 @@ function Map({ stat, byCountries, location, pickedCountry }) {
 
     useEffect(() => {
         setStatistic(stat)
-    // console.log(stat, statictic, 'STAT STATISTIC')
+        // console.log(stat, statictic, 'STAT STATISTIC')
     }, [stat])
 
     const classes = useStyles();
@@ -96,6 +105,20 @@ function Map({ stat, byCountries, location, pickedCountry }) {
         }
     }
 
+    const onMarkerHover = (e) => {
+        return {
+            mouseover: (e) => {
+                e.target.openPopup();
+                console.log(e.target, 'marker hovered')
+
+            },
+        }
+    }
+
+    // marker.on('focus', (e) => {
+    //     console.log(marker, "FOCUS")
+
+    // })
     function getStaticticsValue(item) {
         const { category, timePeriod } = statictic;
         const statType = category === 'confirmed' ? 'cases' : category.toString();
@@ -106,19 +129,26 @@ function Map({ stat, byCountries, location, pickedCountry }) {
         return staticticValue;
     }
 
-    function renderProvinceMarker(item) {
+    function renderProvinceMarker(item, country) {
         const { category, timePeriod } = statictic;
-
         const center = [item.coordinates.latitude, item.coordinates.longitude];
-        let staticticValue = getStaticticsValue(item);
-        const radius = (staticticValue / 10000000) * zoom;
-    //  console.log(staticticValue, item, 'DF;LBNJ;FLSBNF')
-        const backColor = getColor(staticticValue);
-        return <CircleMarker center={center} pathOptions={fillOptions(backColor)} radius={1 * zoom}>
+        // let staticticValue = getStaticticsValue(item);
+        // const radius = (item.stats[category] / 10000000) * zoom;
+        const backColor = getColor(item.stats[category]);
+        return <CircleMarker
+            eventHandlers={{
+                mouseover: (e) => {
+                    e.target.openPopup();
+                },
+                mouseout: (e) => {
+                    e.target.closePopup();
+                }
+            }}
+            ref={marker} center={center} pathOptions={fillOptions(backColor)} radius={1 * zoom}>
             <Popup>
-                {`${category} ${timePeriod}`}:{staticticValue}
+                {`${category} ${timePeriod}`}:{item.stats[category]}
                 <br />
-                {item.country}, {item.province}, {item.county}
+                {country}, {item.province}
             </Popup>
         </CircleMarker>
     }
@@ -126,14 +156,18 @@ function Map({ stat, byCountries, location, pickedCountry }) {
 
     function renderCountryMarker(item, stat, idx, radius) {
         const { category, timePeriod } = stat;
-        // const statType = category === 'confirmed' ? 'cases' : category.toString();
         let staticticValue = getStaticticsValue(item);
-
         const center = [item.countryInfo.lat, item.countryInfo.long];
-        // const radius = item[statType] / 10000000;
         const backColor = getColor(staticticValue);
-        console.log(item)
-        return <CircleMarker center={center} pathOptions={fillOptions(backColor)} radius={radius ? radius : 1 * zoom} key={idx ? idx : staticticValue}>
+        return <CircleMarker
+            eventHandlers={{
+                mouseover: (e) => {
+                    e.target.openPopup();
+                },
+                mouseout: (e) => {
+                    e.target.closePopup();
+                }
+            }} center={center} pathOptions={fillOptions(backColor)} radius={radius ? radius : 1 * zoom} key={idx ? idx : staticticValue}>
             <Popup>
                 {`${category} ${timePeriod}`}:{staticticValue}
                 <br />
@@ -150,24 +184,29 @@ function Map({ stat, byCountries, location, pickedCountry }) {
 
 
     return (
-        < MapContainer className={classes.mapContainer} center={newLocation} minZoom={1} zoom={zoom} scrollWheelZoom={true}>
-            <MapEventHandler setZoom={setZoom} setIsNewLocation={setIsNewLocation} />
-            {country !== null && isNewLocation ? <FlyToLocation position={newLocation} item={country} stat={statictic} /> : null}
-            <MapProvider />
+        <>
+            <h1 onClick={(e) => {
+                onMarkerHover()
+            }} >Map</h1>
+            <MapContainer onClick={(e) => console.log(e.target)} className={classes.mapContainer} center={newLocation} minZoom={1} zoom={zoom} scrollWheelZoom={true}>
+                <MapEventHandler setZoom={setZoom} setIsNewLocation={setIsNewLocation} />
+                {country !== null && isNewLocation ? <FlyToLocation position={newLocation} item={country} stat={statictic} /> : null}
+                <MapProvider />
+                <TileLayer
+                    url="https://{s}.tile.openstreetmap.org/{z}/{x}/{y}.png"
+                    attribution='&copy; <a href="http://osm.org/copyright">OpenStreetMap</a> contributors'
+                />
+                {cases.map((item, idx) => {
+                    if (item.provinces !== null) {
+                        return item.provinces.map((provence) => renderProvinceMarker(provence, item.country))
+                    }
+                    return renderCountryMarker(item, stat, idx)
 
-            <TileLayer
-                url="https://{s}.tile.openstreetmap.org/{z}/{x}/{y}.png"
-                attribution='&copy; <a href="http://osm.org/copyright">OpenStreetMap</a> contributors'
-            />
-            {cases.map((item, idx) => {
-                if (item.provinces !== null) {
-                    return item.provinces.map((provence) => renderProvinceMarker(provence))
-                }
-                return renderCountryMarker(item, stat, idx)
+                })}
+                <Legend onClick={() => console.log('LEGEND CLICKED')} />
+            </MapContainer >
+        </>
 
-            })}
-            <Legend />
-        </MapContainer >
     )
 }
 
