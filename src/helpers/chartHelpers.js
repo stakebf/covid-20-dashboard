bb6cdb2c4fc1fe81403c544259f0bacd50709311
Volkau Aliaksey <stakebf@gmail.com>
2020-12-23 16:30:15 +0300

    feat: add helpers for work with data

--- src/helpers/chartHelpers.js
+++ src/helpers/chartHelpers.js
@@ -1,29 +1,121 @@
-const setChartAxis = (dates, cases, deaths, recovered, countryName) => {
+import { caseCalculationFormula } from './getInfoAboutCountry';
+
+const convertData = (data, is100K, population) => {
+  return data.map((item, idx) => {
+    let currentItem;
+    if (idx === 0) {
+      currentItem = 0;
+    } else {
+      currentItem = item - data[idx - 1];
+      currentItem = currentItem < 0 ? 0 : currentItem;
+
+      if (is100K) {
+        currentItem = (caseCalculationFormula(item, population) - caseCalculationFormula(data[idx - 1], population)).toFixed(2);
+        currentItem = currentItem < 0 ? 0 : currentItem;
+      }
+    }
+    return currentItem;
+  })
+}
+
+const setChartAxis = (dates, cases, deaths, recovered, population) => {
   const data = {
     labels: dates,
     datasets: [
       {
-        label: 'cases',
+        label: 'cases/total',
         data: cases,
         fill: false,
-        backgroundColor: '#62be62',
+        backgroundColor: 'darkgreen',
+        borderColor: 'darkgreen',
+        yAxisID: 'y-axis-1',
+      },
+      {
+        label: 'cases/today',
+        data: convertData(cases),
+        fill: false,
+        backgroundColor: 'green',
         borderColor: 'green',
         yAxisID: 'y-axis-1',
       },
       {
-        label: 'deaths',
+        label: 'cases/total/100K',
+        data: convertData(cases, true, population),
+        fill: false,
+        backgroundColor: '#adff2f',
+        borderColor: '#adff2f',
+        yAxisID: 'y-axis-1',
+      },
+      {
+        label: 'cases/today/100K',
+        data: convertData(convertData(cases), true, population),
+        fill: false,
+        backgroundColor: '#14f714',
+        borderColor: '#14f714',
+        yAxisID: 'y-axis-1',
+      },
+      {
+        label: 'deaths/total',
         data: deaths,
         fill: false,
-        backgroundColor: 'rgb(255, 99, 132)',
+        backgroundColor: 'darkred',
+        borderColor: 'darkred',
+        yAxisID: 'y-axis-1',
+      },
+      {
+        label: 'deaths/today',
+        data: convertData(deaths),
+        fill: false,
+        backgroundColor: 'red',
         borderColor: 'red',
         yAxisID: 'y-axis-1',
       },
       {
-        label: 'recovered',
+        label: 'deaths/total/100K',
+        data: convertData(deaths, true, population),
+        fill: false,
+        backgroundColor: 'indianred',
+        borderColor: 'indianred',
+        yAxisID: 'y-axis-1',
+      },
+      {
+        label: 'deaths/today/100K',
+        data: convertData(convertData(deaths), true, population),
+        fill: false,
+        backgroundColor: 'palevioletred',
+        borderColor: 'palevioletred',
+        yAxisID: 'y-axis-1',
+      },
+      {
+        label: 'recovered/total',
         data: recovered,
         fill: false,
-        backgroundColor: 'rgb(54, 162, 235)',
+        backgroundColor: 'darkblue',
+        borderColor: 'darkblue',
+        yAxisID: 'y-axis-1',
+      },
+      {
+        label: 'recovered/today',
+        data: convertData(recovered),
+        fill: false,
+        backgroundColor: 'blue',
         borderColor: 'blue',
+        yAxisID: 'y-axis-1',
+      },
+      {
+        label: 'recovered/total/100K',
+        data: convertData(recovered, true, population),
+        fill: false,
+        backgroundColor: 'cadetblue',
+        borderColor: 'cadetblue',
+        yAxisID: 'y-axis-1',
+      },
+      {
+        label: 'recovered/today/100K',
+        data: convertData(convertData(recovered), true, population),
+        fill: false,
+        backgroundColor: 'dodgerblue',
+        borderColor: 'dodgerblue',
         yAxisID: 'y-axis-1',
       },
     ],
@@ -32,26 +124,30 @@ const setChartAxis = (dates, cases, deaths, recovered, countryName) => {
   return data;
 }
 
-const getCountriesData = (
-    activeCountry, 
-    byHistoricalAll, 
-    byHistoricalCountry
-) => {
-  console.log(activeCountry);
-  console.log(byHistoricalAll);
-  console.log(byHistoricalCountry);
-  const countryName = Object.keys(activeCountry).length ? activeCountry.country : 'World data';
-  console.log(countryName, 'countryName');
-  const data = Object.keys(byHistoricalCountry).length ? byHistoricalCountry.timeline : byHistoricalAll;
+const prepareCountiesData = (data, population) => {
   const dates = [];
   const cases = [];
   Object.entries(data.cases).forEach(([key, value]) => {
     cases.push(value);
     dates.push(key);
   });
+
   const deaths = Object.values(data.deaths);
   const recovered = Object.values(data.recovered);
-  return setChartAxis(dates, cases, deaths, recovered, countryName);
+
+  return setChartAxis(dates, cases, deaths, recovered, population);
+}
+
+const getCountriesData = (
+    byHistoricalAll, 
+    byHistoricalCountry,
+    activeCountry,
+    byAllCases
+) => {
+  const data = Object.keys(byHistoricalCountry).length && !byHistoricalCountry.message ? byHistoricalCountry.timeline : byHistoricalAll;
+  const population = Object.keys(byHistoricalCountry).length && !byHistoricalCountry.message ? activeCountry.population : byAllCases.population;
+
+  return prepareCountiesData(data, population);
 }
 
 export { getCountriesData };
