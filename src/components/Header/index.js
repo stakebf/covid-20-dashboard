0208409510a9c3324b86ed14b46199f17ea2f2af
Volkau Aliaksey <stakebf@gmail.com>
2020-12-24 04:10:44 +0300

    add last update date

--- src/components/Header/index.js
+++ src/components/Header/index.js
@@ -1,12 +1,23 @@
 import React from 'react';
+import { connect } from 'react-redux';
+
 import classes from './Header.module.scss';
 
-const Header = () => {
+const Header = ({ byAllCases }) => {
+  console.log(byAllCases);
+  const date = `${new Date(byAllCases.updated).toDateString()} ${new Date(byAllCases.updated).toLocaleTimeString()}`;
   return (
     <header className={classes.header}>
       <h1>COVID-20 Dashboard</h1>
+      <span>Last update: {!Object.keys(byAllCases).length ? new Date().toDateString() : date}</span>
     </header>
   );
 };
 
-export default Header;
+const mapStateToProps = (state) => {
+  return {
+    byAllCases: state.byAllCases
+  }
+}
+
+export default connect(mapStateToProps)(Header);
